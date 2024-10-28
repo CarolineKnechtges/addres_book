a2c94e90d6354d4a2fd05b2c671af22ecfdbc4da
黄腾辉 <huangtenghui@xmpaoyou.com>
2024-10-28 17:10:27 +0800

    解决跨域问题

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module address_book
 go 1.19
 
 require (
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
--- routes/router.go
+++ routes/router.go
@@ -1,9 +1,11 @@
-// Package routes/router.go
+// routes/router.go
 package routes
 
 import (
 	"address_book/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 const (
@@ -13,6 +15,16 @@ const (
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	// 配置 CORS 中间件
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // 允许的前端地址
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// 定义路由
 	router.GET(usersPath, controllers.GetUsers)
